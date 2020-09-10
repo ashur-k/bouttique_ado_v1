4e84c23af09e6aab00309b2274a76bb8e0d34ccb
ashur-k <ashurkanwal@yahoo.com>
2020-09-10 12:09:56 +0000

    Toasts out of page alignment error fixed

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -133,8 +133,8 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-            # 'NAME': BASE_DIR / 'db.sqlite3',
+            # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
 
